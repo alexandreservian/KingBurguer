b3558cb1d44224177cf4a316f4ac6be70645c50e
alexandreservian <alexandreservian@gmail.com>
2026-03-10 13:27:17 -0300

    refactor: adicionando um componente de forma separada

--- KingBurguer/Signin/View/SigninViewController.swift
+++ KingBurguer/Signin/View/SigninViewController.swift
@@ -9,7 +9,12 @@ import Foundation
 import UIKit
 
 class SigninViewController: UIViewController {
-    let email: UITextField = UITextField()
+    let email: UITextField = {
+        let ed = UITextField()
+        ed.backgroundColor = .white
+        ed.placeholder = "Email"
+        return ed
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,8 +22,7 @@ class SigninViewController: UIViewController {
         view.backgroundColor = .orange
         
         view.addSubview(email)
-        email.backgroundColor = .white
-        email.placeholder = "Email"
+        
         email.frame = CGRect(x: 0, y: view.bounds.height / 2, width: view.bounds.size.width, height: 50)
     }
 }
