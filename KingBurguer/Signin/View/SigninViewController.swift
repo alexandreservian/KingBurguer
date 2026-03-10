61bdb0605482b0505d6cf8f0fb9a07db5156b08c
alexandreservian <alexandreservian@gmail.com>
2026-03-10 13:25:49 -0300

    refactor: adicionando o primeiro componente em tela

--- KingBurguer/Signin/View/SigninViewController.swift
+++ KingBurguer/Signin/View/SigninViewController.swift
@@ -9,5 +9,16 @@ import Foundation
 import UIKit
 
 class SigninViewController: UIViewController {
+    let email: UITextField = UITextField()
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .orange
+        
+        view.addSubview(email)
+        email.backgroundColor = .white
+        email.placeholder = "Email"
+        email.frame = CGRect(x: 0, y: view.bounds.height / 2, width: view.bounds.size.width, height: 50)
+    }
 }
