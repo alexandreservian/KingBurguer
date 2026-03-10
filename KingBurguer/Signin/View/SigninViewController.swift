7b01abb9288b4420a24fc150bec33a854a80c540
alexandreservian <alexandreservian@gmail.com>
2026-03-10 13:09:48 -0300

    refactor: definindo a ViewController como a primeira view do projeto

---
+++ KingBurguer/Signin/View/SigninViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SigninViewController.swift
+//  KingBurguer
+//
+//  Created by Alexandre  Servian on 10/03/26.
+//
+
+import Foundation
+import UIKit
+
+class SigninViewController: UIViewController {
+    
+}
